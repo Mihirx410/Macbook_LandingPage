0613a65292a59ac72ca5146deec6e65752e1b127
Mihir Dodiya <mihirdodiya0077@gmail.com>
2025-04-11 14:20:53 +0530

    created 3d model and scroll animation base

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
 import './App.css'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
+import Macbook from './Macbook'
 
-function App() {
-  const [count, setCount] = useState(0)
+// you can't print html inside the canvas
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas camera={{fov: 12, position:[0,4,220]}}>
+
+        {/* <OrbitControls/> */}
+
+
+      {/* Scroll from drei for scroll bar */}
+      <ScrollControls pages={4}>
+        <Macbook/>
+        {/* For HDRI lighting from link */}
+        <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/symmetrical_garden_02_4k.hdr']} />
+      </ScrollControls>
+
+        
+    </Canvas>
   )
 }
 
---
+++ src/Macbook.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './App.css'
+import { useGLTF, useScroll, useTexture } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+
+// this is how you can add a 3d model in react fiber
+
+const Macbook = () => {
+  let model = useGLTF("./mac.glb")
+  const texture = useTexture("./red.jpg")
+
+  
+  // meshes are the thing from which model is created like borders, edges are called meshes
+  let meshes = {};
+
+  // logic to fold the macbook
+  model.scene.traverse((e)=>{
+      meshes[e.name] = e;
+  });
+  // here is all the parts are shown from which the model is created and we'll use them to fold the laptop, and adding the texture
+  console.log(meshes,"meshes object");
+
+  // scroll animation
+  const data = useScroll();
+
+  // use frame is best because it works on systems frame
+  useFrame((state, delta)=>{
+    meshes.screen.rotation.x= THREE.MathUtils.degToRad(180 - data.offset * 90 )
+  })
+
+
+  return (
+    <group position={[0,-10,20]}>
+    <primitive object={model.scene}/>
+    </group>
+  )
+}
+
+export default Macbook
